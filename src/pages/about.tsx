6ddaba8f3c25fe1d32339d8d63a04a3ba28f4470
shinta <flekystyley@gmail.com>
2023-11-27 14:47:14 +0900

    update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,17 @@
 import { fira } from "@utils/font";
-import Sidebar from "./header";
+import Header from "./header"
 
 export default function About() {
     return (
-        <div>
-            <Sidebar />
-        </div>
+        <Header>
+            <p className={`${fira.className} text-xs`}>
+                development and CEO of runetale,<br />
+                interest include wasm, compilier, kernel, security, and<br />
+                p2p mesh network.<br />
+                i often paint and also play bass, guitar, and digital-synthesizer,<br />
+                so i may upload some of my compositions.<br />
+            </p>
+        </Header>
     )
 }
 
