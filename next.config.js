c85d12b356b41ea6d28aaaa2ef0a6a17aba815f7
shinta <flekystyley@gmail.com>
2023-11-28 18:15:34 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  pageExtensions: ['index.tsx', 'index.ts', 'index.jsx', 'index.js'],
+  // pageExtensions: ['index.tsx', 'index.ts', 'index.jsx', 'index.js'],
   reactStrictMode: false,
   webpack(config, options) {
     const { isServer } = options;
