275fc4635aa9ef2c9ba2d073c8925a0d5d12e7c7
shinta <flekystyley@gmail.com>
2023-12-02 23:23:51 +0900

    tiny fix

--- src/pages/misc.tsx
+++ src/pages/misc.tsx
@@ -9,9 +9,15 @@ export default function Misc() {
                 <p>misc</p>
             </p>
             <div className="grid grid-flow-row auto-rows-max gap-2">
-                <div className="grid grid-cols-2 items-center justify-between gap-20">
+                <div className="grid grid-cols-2 items-center justify-between gap-6">
+                    <Link href="/misc/grumble" className={`${fira.className} text-lg hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
+                        grumble, wasm kernel p2p
+                    </Link>
+                    <p className={`${fira.className} text-xs text-gray-400`}>
+                        nov 25, 2023<br />
+                    </p>
                     <Link href="/misc/new-normal" className={`${fira.className} text-lg hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
-                        UDP Hole Punching
+                        new normal
                     </Link>
                     <p className={`${fira.className} text-xs text-gray-400`}>
                         nov 25, 2023<br />
--- src/pages/writing.tsx
+++ src/pages/writing.tsx
@@ -9,9 +9,9 @@ export default function Writing() {
                 writing<br />
             </p>
             <div className="grid grid-flow-row auto-rows-max gap-2">
-                <div className="grid grid-cols-2 items-center justify-between gap-20">
-                    <Link href="/writing/new-normal" className={`${fira.className} text-lg hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
-                        The New Normal
+                <div className="grid grid-cols-2 items-center justify-between gap-6">
+                    <Link href="/writing/udp-hole-punching" className={`${fira.className} text-lg hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
+                        udp hole punching
                     </Link>
                     <p className={`${fira.className} text-xs text-gray-400`}>
                         nov 25, 2023<br />
