d4acbd3db2e616e897be2c102dc496593c147c83
shinta <flekystyley@gmail.com>
2023-12-02 23:11:27 +0900

    adde new impr

--- src/pages/improvisation.tsx
+++ src/pages/improvisation.tsx
@@ -1,38 +1,34 @@
 import { fira } from "@utils/font";
 import Header from "./header"
-import { useRef, useState } from "react";
-import MyFile from '@assets/imp25112023.mp3';
+import Impr from '@assets/imp25112023.mp3';
+import Gangsta from '@assets/gangsta.mp3';
 
 export default function Improvisation() {
-    const audioRef = useRef<HTMLVideoElement>()
-    const [isPlaying, setPlaying] = useState(false)
-
-    const play = () => {
-        if (!isPlaying) {
-            audioRef.current?.play();
-            setPlaying(true)
-        } else {
-            audioRef.current?.pause();
-            setPlaying(true)
-        }
-    }
-
     return (
         <Header>
             <p className={`${fira.className}`}>
                 improvisation<br />
             </p>
-            <div className="grid grid-flow-row auto-rows-max gap-5">
-                <div className="flex justify-end items-center gap-4">
-                    <audio src={MyFile} controls className="text-xs "></audio>
-                    <div className="flex flex-col items-end gap-2">
-                        <p className={`${fira.className} text-xs text-gray-400`}>
-                            nov 27, 2023<br />
-                        </p>
-                        <a href={MyFile} download className="hover:text-gray-500 underline underline-offset-8">dowload</a>
+            <div className="grid auto-rows-max gap-12">
+                <div className="flex flex-col gap-4">
+                    <p className={`${fira.className} text-md text-gray-400`}>
+                        title: 'gangsta', dec 1, 2023<br />
+                    </p>
+                    <div className="flex flex-row gap-4 justify-start items-center">
+                        <audio src={Gangsta} controls className="text-xs "></audio>
+                        <a href={Gangsta} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
+                    </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                    <p className={`${fira.className} text-md text-gray-400`}>
+                        title: 'impr', nov 27, 2023<br />
+                    </p>
+                    <div className="flex flex-row gap-4 justify-start items-center">
+                        <audio src={Impr} controls className="text-xs "></audio>
+                        <a href={Impr} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
                     </div>
                 </div>
             </div>
-        </Header>
+        </Header >
     )
 }
