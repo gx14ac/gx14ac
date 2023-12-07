bdacc330cc8cd313128f8eef8c505f4983d93a19
shinta <flekystyley@gmail.com>
2023-12-07 13:00:19 +0900

    add asset

--- src/pages/improvisation.tsx
+++ src/pages/improvisation.tsx
@@ -1,7 +1,7 @@
 import { fira } from "@utils/font";
 import Header from "./header"
 import Impr from '@assets/imp25112023.mp3';
-import Gangsta from '@assets/gangsta.mp3';
+import OneScene from '@assets/one-scene.mp3';
 
 export default function Improvisation() {
     return (
@@ -12,11 +12,11 @@ export default function Improvisation() {
             <div className="grid auto-rows-max gap-12">
                 <div className="flex flex-col gap-4">
                     <p className={`${fira.className} text-md text-gray-400`}>
-                        title: 'gangsta', dec 1, 2023<br />
+                        title: 'one scene', dec 1, 2023<br />
                     </p>
                     <div className="flex flex-row gap-4 justify-start items-center">
-                        <audio src={Gangsta} controls className="text-xs "></audio>
-                        <a href={Gangsta} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
+                        <audio src={OneScene} controls className="text-xs "></audio>
+                        <a href={OneScene} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
                     </div>
                 </div>
                 <div className="flex flex-col gap-4">
