867a7c86df9dd3e4dc5c4845e75c7e5a5ab3dbea
shinta <flekystyley@gmail.com>
2023-12-11 16:10:46 +0900

    fix fonts

--- src/pages/misc/index.tsx
+++ src/pages/misc/index.tsx
@@ -8,7 +8,7 @@ export default function Misc() {
             <div className="mt-24 sm:ml-8 mini:ml-6 mini:mr-10">
                 <div className="flex flex-col gap-5">
                     <div className="flex-row flex items-center gap-6 justify-between">
-                        <Link href="/misc/grumble" className={`${fira.className} text-lg hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
+                        <Link href="/misc/grumble" className={`${fira.className} text-xl hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
                             grumble, wasm kernel p2p
                         </Link>
                         <p className={`${fira.className} text-xs text-gray-400`}>
@@ -16,7 +16,7 @@ export default function Misc() {
                         </p>
                     </div>
                     <div className="flex-row flex items-center gap-6 justify-between">
-                        <Link href="/misc/new-normal" className={`${fira.className} text-lg hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
+                        <Link href="/misc/new-normal" className={`${fira.className} text-xl hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
                             new normal
                         </Link>
                         <p className={`${fira.className} text-xs text-gray-400`}>
