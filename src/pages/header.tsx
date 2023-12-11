178dbd07a79f1243fbee43c627eff2b2b93040e6
shinta <flekystyley@gmail.com>
2023-12-11 23:16:46 +0900

    fix responsive

--- src/pages/header.tsx
+++ src/pages/header.tsx
@@ -10,8 +10,8 @@ type ComponentProps = {
 export default function Header({ children }: ComponentProps) {
     const router = useRouter()
     return (
-        <div className="md:flex md:justify-start md:items-start md:gap-40 md:mt-16 md:ml-36 mini:ml-4">
-            <div className="md:grid md:gap-4 md:mt-6 sm:mt-10 sm:flex sm:items-center sm:justify-evenly">
+        <div className="md:flex md:justify-start md:items-start md:gap-36 lg:gap-30 md:mt-32 md:ml-20 lg:ml-44 mini:ml-4">
+            <div className="md:grid md:gap-6 md:mt-6 sm:mt-10 sm:flex sm:items-center sm:justify-evenly mini:ml-7 sm:mr-40">
                 <Image
                     src="/assets/artwork.png"
                     className="mini:invisible md:visible sm:visible"
@@ -19,23 +19,23 @@ export default function Header({ children }: ComponentProps) {
                     width={100}
                     height={100}
                 />
-                <Link href="/" className={`${fira.className} ml-5 hover:text-blue-600`} onClick={() => router.push('/')}>
+                <Link href="/" className={`${fira.className} ml-5 text-lg hover:text-blue-600`} onClick={() => router.push('/')}>
                     About
                 </Link>
-                <Link href="/writing" className={`${fira.className} ml-5 hover:text-blue-600`} onClick={() => router.push('/writing')}>
+                <Link href="/writing" className={`${fira.className} ml-5 text-lg hover:text-blue-600`} onClick={() => router.push('/writing')}>
                     Writing
                 </Link>
-                <Link href="/misc" className={`${fira.className} ml-5 hover:text-blue-600`} onClick={() => router.push('/misc')}>
+                <Link href="/misc" className={`${fira.className} ml-5 text-lg hover:text-blue-600`} onClick={() => router.push('/misc')}>
                     Misc
                 </Link>
-                <Link href="/artwork" className={`${fira.className} ml-5 hover:text-blue-600`} onClick={() => router.push('/artwork')}>
+                <Link href="/artwork" className={`${fira.className} ml-5 text-lg hover:text-blue-600`} onClick={() => router.push('/artwork')}>
                     Artwork
                 </Link>
-                <Link href="/improvisation" className={`${fira.className} ml-5 hover:text-blue-600`} onClick={() => router.push('improvisation')}>
-                    Improvisation
+                <Link href="/improvisation" className={`${fira.className} ml-5 text-lg hover:text-blue-600`} onClick={() => router.push('improvisation')}>
+                    Impr
                 </Link>
             </div>
-            <div className="mt-8">
+            <div className="mt-8 mini:mt-16">
                 {children}
             </div>
         </div >
