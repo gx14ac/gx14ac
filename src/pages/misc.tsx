46d4bc9e37040f65e63ff2f8902d5882d35df9fc
shinta <flekystyley@gmail.com>
2023-12-12 16:00:20 +0900

    add new music

---
+++ src/pages/misc.tsx
@@ -0,0 +1,23 @@
+import Header from "./header"
+import { fira } from "@utils/font"
+import Link from "next/link"
+
+export default function Misc() {
+    return (
+        <Header>
+            <p className={`${fira.className}`}>
+                <p>misc</p>
+            </p>
+            <div className="grid grid-flow-row auto-rows-max gap-2">
+                <div className="grid grid-cols-2 items-center justify-between gap-20">
+                    <Link href="/misc/new-normal" className={`${fira.className} text-lg hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
+                        UDP Hole Punching
+                    </Link>
+                    <p className={`${fira.className} text-xs text-gray-400`}>
+                        nov 25, 2023<br />
+                    </p>
+                </div>
+            </div>
+        </Header>
+    )
+}
