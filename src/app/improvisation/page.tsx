ea13c3e19a107d86f18811bf6368ecada2a81aa9
shinta <flekystyley@gmail.com>
2023-12-15 13:36:30 +0900

    added new png

--- src/app/improvisation/page.tsx
+++ src/app/improvisation/page.tsx
@@ -3,13 +3,23 @@ import { fira } from "@utils/font";
 import Header from "@components/Header"
 import Impr from '@assets/imp25112023.mp3';
 import OneScene from '@assets/one-scene.mp3';
-import Maenisusumu from '@assets/maenisusumusikanai.mp3';
+import Hbtm from '@assets/hbtm.mp3';
 import Zerotier from '@assets/zerotier.mp3';
+import Dosed from '@assets/dosed.mp3';
 
 export default function Improvisation() {
     return (
         <Header>
             <div className="grid auto-rows-max gap-12 justify-center">
+                <div className="flex flex-col gap-4">
+                    <p className={`${fira.className} text-md text-gray-400`}>
+                        title: 'dosed', dec 15, 2023<br />
+                    </p>
+                    <div className="flex flex-row gap-4 justify-start items-center">
+                        <audio src={Dosed} controls className="text-xs "></audio>
+                        <a href={Dosed} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
+                    </div>
+                </div>
                 <div className="flex flex-col gap-4">
                     <p className={`${fira.className} text-md text-gray-400`}>
                         title: 'zerotier', dec 14, 2023<br />
@@ -21,11 +31,11 @@ export default function Improvisation() {
                 </div>
                 <div className="flex flex-col gap-4">
                     <p className={`${fira.className} text-md text-gray-400`}>
-                        title: 'maeni susumu sikanai 26', dec 9, 2023<br />
+                        title: 'hbtm 26', dec 9, 2023<br />
                     </p>
                     <div className="flex flex-row gap-4 justify-start items-center">
-                        <audio src={Maenisusumu} controls className="text-xs "></audio>
-                        <a href={Maenisusumu} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
+                        <audio src={Hbtm} controls className="text-xs "></audio>
+                        <a href={Hbtm} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
                     </div>
                 </div>
                 <div className="flex flex-col gap-4">
