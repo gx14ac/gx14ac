62886fc7b37e659680a2192a730d22ae64c945b6
shinta <flekystyley@gmail.com>
2024-02-11 12:18:17 +0900

    update

--- src/app/improvisation/page.tsx
+++ src/app/improvisation/page.tsx
@@ -6,11 +6,21 @@ import OneScene from '@assets/one-scene.mp3';
 import Hbtm from '@assets/hbtm.mp3';
 import Zerotier from '@assets/zerotier.mp3';
 import Dosed from '@assets/dosed.mp3';
+import Jaco from '@assets/likejaco.mp3';
 
 export default function Improvisation() {
     return (
         <Header>
             <div className="grid auto-rows-max gap-12 justify-center">
+                <div className="flex flex-col gap-4">
+                    <p className={`${fira.className} text-md text-gray-400`}>
+                        title: 'danche', feb 11, 2024<br />
+                    </p>
+                    <div className="flex flex-row gap-4 justify-start items-center">
+                        <audio src={Jaco} controls className="text-xs "></audio>
+                        <a href={Jaco} download className="text-xs hover:text-gray-500 underline underline-offset-8">download</a>
+                    </div>
+                </div>
                 <div className="flex flex-col gap-4">
                     <p className={`${fira.className} text-md text-gray-400`}>
                         title: 'dosed', dec 15, 2023<br />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-'use client';
+'use'
 import Image from "next/image"
 import { useRouter } from "next/navigation"
 import { fira } from "@utils/font"
@@ -45,3 +45,4 @@ export default function Header({ children }: ComponentProps) {
         </div >
     )
 }
+
