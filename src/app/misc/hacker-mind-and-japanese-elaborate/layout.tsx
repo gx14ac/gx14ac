edef9010fde27afda3494b1cc60e55ce5512ff6a
shinta <flekystyley@gmail.com>
2024-03-09 16:26:10 +0900

    fix

--- src/app/misc/hacker-mind-and-japanese-elaborate/layout.tsx
+++ src/app/misc/hacker-mind-and-japanese-elaborate/layout.tsx
@@ -7,7 +7,7 @@ export default function MdxLayout({ children }: { children: React.ReactNode }) {
     const router = useRouter()
     return (
         <Header>
-            <div className="prose dark:prose-invert text-slate-600 dark:text-slate-400 tracking-wide ml-6 mr-6 mt-10 mb-6" style={{ color: 'white' }}>{children}</div>
+            <div className="prose dark:prose-invert text-slate-600 dark:text-slate-400 tracking-wide ml-4 mr-6 mt-10 mb-6" style={{ color: 'white' }}>{children}</div>
             <Image
                 src="/assets/first-hike7.png"
                 className=""
