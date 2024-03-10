2ae8834e390dfc779ae28ecd1b63d7f44f7013f1
shinta <flekystyley@gmail.com>
2024-03-10 22:28:17 +0900

    add

--- src/app/misc/page.tsx
+++ src/app/misc/page.tsx
@@ -9,6 +9,14 @@ export default function Misc() {
             <div className="mt-24 sm:ml-8 mini:ml-6 mini:mr-10">
                 <div className="flex flex-col gap-5">
                     <div className="flex-row flex items-center gap-6 justify-between">
+                        <Link href="/misc/ajisashi" className={`${fira.className} text-xl hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
+                            ajisashi.
+                        </Link>
+                        <p className={`${fira.className} text-xs text-gray-400`}>
+                            mar 10, 2024<br />
+                        </p>
+                    </div>
+                    <div className="flex-row flex items-center gap-6 justify-between">
                         <Link href="/misc/terrible-japan" className={`${fira.className} text-xl hover:text-gray-500 underline underline-offset-8`} onClick={() => { }}>
                             terrible japan.
                         </Link>
